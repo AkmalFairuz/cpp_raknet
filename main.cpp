b1fa3d705115ee01df0776c8d05b5e19487f0df5
AkmalFairuz <akmalf475@gmail.com>
2025-09-08 23:40:45 +0700

    Add Windows-specific WSAStartup initialization for network listener

--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
-//
-// Created by Akmal Fairuz on 05/09/25.
-//
-
 #include <format>
 
 #include "Listener.h"
 #include "memory"
 
 int main() {
+#ifdef _WIN32
     WSADATA wsaData;
     int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
         throw std::runtime_error("WSAStartup failed: " + std::string(strerror(iResult)));
     }
+#endif
 
     auto listener = std::make_unique<RakNet::Listener>("0.0.0.0", 19130);
     listener->setPongData(std::format("MCPE;RakNet-CPP;827;1.21.101;123;124;{};RakNet-CPP;Creative;1;{};{};0;", listener->getId(), listener->getListenPort(), listener->getListenPort()));
